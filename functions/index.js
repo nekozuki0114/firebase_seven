89a789455a203921f1bed418c1b319ee6f2f8059
nekozuki0114 <39252319+nekozuki0114@users.noreply.github.com>
2019-07-19 23:23:02 +0900

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
+const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
-const functions = require('firebase-functions');
 
 const axios = require('axios');
 let item;
